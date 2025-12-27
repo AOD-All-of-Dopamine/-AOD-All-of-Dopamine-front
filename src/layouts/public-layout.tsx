0c49256803c3f768036b3fd15c485c2478c44cfa
Kimyudin <163673413+KimYujin22@users.noreply.github.com>
2025-12-27 12:02:58 +0900

    feat: 로그인/회원가입 창에서 네비바 숨기도록 수정

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,13 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { matchPath, Outlet, useLocation } from "react-router-dom";
 import NavigationBar from "../components/common/NavigationBar";
 
 function PublicLayout() {
+  const location = useLocation();
+  const showNav = ["/home", "/explore", "/ranking", "/new", "/profile/*"];
+
+  const showNavBar = showNav.some((pattern) =>
+    matchPath({ path: pattern, end: false }, location.pathname)
+  );
+
   return (
     <div className="flex justify-center min-h-screen">
       <div className="relative w-full min-h-screen pb-410">
         {/* pb-32 = 128px padding-bottom (NavigationBar 높이 + 충분한 여유 공간) */}
         <Outlet />
-        <NavigationBar />
+        {showNavBar && <NavigationBar />}
       </div>
     </div>
   );
